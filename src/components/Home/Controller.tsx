83806f252726582b2523af58bc0c56cedbd91406
seiya_tagami <bepozurapu274@gmail.com>
2023-05-08 16:10:31 +0900

    feat: controller

--- src/components/Home/Controller.tsx
+++ src/components/Home/Controller.tsx
@@ -1,4 +1,4 @@
-import { type Signal, component$, useSignal } from '@builder.io/qwik';
+import { component$, useSignal, useStore } from '@builder.io/qwik';
 import Loading from '../utils/Loading';
 import TodoCard from './TodoCard';
 
@@ -16,21 +16,24 @@ interface TranslatedText {
   to: string
 }
 
-export const genTodoFromAPI = async (lang: Signal<string>) => {
+export const genTodoFromAPI = async (lang: string) => {
   /**
    * Bored API を叩く
    */
   const generatedTodoText = await fetch(import.meta.env.PUBLIC_BORED_API_END_POINT, {
     headers: { Accept: 'application/json' },
   });
-  const { activity } = (await generatedTodoText.json()) as GenTodo;
+  const res = (await generatedTodoText.json()) as GenTodo;
 
   /**
    * Azure Cognitive Translator API を叩く
    */
   const data = [
     {
-      "text": activity
+      "text": res.activity
+    },
+    {
+      "text": res.type
     }
   ];
 
@@ -43,39 +46,42 @@ export const genTodoFromAPI = async (lang: Signal<string>) => {
     },
     body: JSON.stringify(data)
   };
-  const response = await fetch(`${import.meta.env.PUBLIC_AZURE_COGNITIVE_TRANSLATOR_END_POINT}to=${lang.value}`, options);
+  const response = await fetch(`${import.meta.env.PUBLIC_AZURE_COGNITIVE_TRANSLATOR_END_POINT}to=${lang}`, options);
   const result = await response.json();
-  console.log(result[0])
+  console.log(result)
   return result as [
     {
       translations: TranslatedText[]
+    },
+    {
+      translations: TranslatedText[]
     }
-  ];
+  ]
 }
 
 export default component$(() => {
-  const text = useSignal<Promise<string> | string | undefined>("Let's find what to do next!");
-  const lang = useSignal<string>("ja");
+  const todoData = useStore<{ text: Promise<string> | string | undefined, type: Promise<string> | string | undefined, lang: string }>({ text: "Let's find what to do next!", type: "", lang: "ja" })
   const loading = useSignal<boolean>(false)
 
   return (
-    <div class='w-[560px] mx-auto mt-8'>
+    <div class='w-[520px] mx-auto mt-8'>
       <div class="flex items-center gap-2 text-2xl">
         <button class="p-2 text-3xl font-semibold text-white bg-purple-500 rounded-md italic hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300" onClick$={async () => {
           loading.value = true;
-          const result = await genTodoFromAPI(lang);
-          text.value = result[0].translations[0].text;
+          const result = await genTodoFromAPI(todoData.lang);
+          todoData.text = result[0].translations[0].text;
+          todoData.type = result[1].translations[0].text;
           loading.value = false;
         }}>GEN🚀</button>
         <p>in</p>
-        <select name="lang_select" class='p-2 bg-gray-200 rounded-lg text-teal-500' onChange$={(e) => lang.value = e.target.value}>
+        <select name="lang_select" class='p-2 bg-gray-200 rounded-lg text-teal-500' onChange$={(e) => todoData.lang = e.target.value}>
           <option value="ja">Japanese</option>
           <option value="en">English</option>
           <option value="zh-Hans">Chinese Simplified</option>
         </select>
       </div>
       <div class={"w-full h-[200px] flex justify-center items-center mt-8"}>
-        {loading.value ? <Loading /> : <TodoCard text={text.value} />}
+        {loading.value ? <Loading /> : <TodoCard text={todoData.text} type={todoData.type} />}
       </div>
     </div>
   );
--- src/components/Home/TodoCard.tsx
+++ src/components/Home/TodoCard.tsx
@@ -1,15 +1,18 @@
 import { component$ } from "@builder.io/qwik";
 
 interface TodoCardProps {
-  text?: Promise<string> | string
+  text?: Promise<string> | string,
+  type?: Promise<string> | string,
 }
 export default component$<TodoCardProps>((props) => {
-  const { text } = props;
+  const { text, type } = props;
   return (
-    <div class="w-full h-full p-6 bg-white border border-gray-200 rounded-lg shadow">
+    <div class="w-full m-h-full p-6 bg-white border border-gray-200 rounded-lg shadow">
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{text}</h5>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
-      <div class="flex justify-center gap-10">
+      {type && (
+        <span class="bg-blue-100 text-blue-800 text-[18px] font-medium mr-2 px-2.5 py-0.5 rounded-full">{type}</span>
+      )}
+      <div class="flex justify-end">
         {text !== "Let's find what to do next!" && (<button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-teal-600 rounded-lg hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
           Add
           <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
